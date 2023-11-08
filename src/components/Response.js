7724781150a8b3fbce24defbb39f4126e0ff2090
mattk1205 <mkucera@smu.edu>
2023-11-08 05:31:13 -0600

    q&a components added

---
+++ src/components/Response.js
@@ -0,0 +1,21 @@
+import React from 'react';
+export default function ResponseComponent ({ responses })  {
+        const splitResponses = responses.split(/(?=\d\. )/g);
+        return (
+            <div>
+                <div  style={{margin: '1em'}}>
+                    <strong>Responses:</strong>
+                </div>
+                {splitResponses.map((part, index) => {
+                    if (part) {
+                        return (
+                            <div key={index} style={{marginBottom: '1em'}}>
+                                {part}
+                            </div>
+                        );
+                    }
+                    return null;
+                })}
+            </div>
+        );
+    }
