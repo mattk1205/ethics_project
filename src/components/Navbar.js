f0969cf6ab216c32717e3ae74609805861c4b77d
thannansmu <thannan@smu.edu>
2023-11-16 10:17:54 -0600

    Revert "Merge remote-tracking branch 'origin/main'"
    This reverts commit 917d0b249a1f37fae07742acbc90ac4a69bd41ac, reversing
    changes made to 345cec80ca207a43c14108050d63a0c906c8c91e.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,11 +2,9 @@ import React, { useState } from "react";
 import { Link, useNavigate } from 'react-router-dom';
 import "../styles/Navbar.css";
 import CompanyLogo from "../assets/Company-Logo.png";
-import CompanyLogoHover from "../assets/Company-Logo-Hover.png";
 
 function Navbar() {
   const [openLinks, setOpenLinks] = useState(false);
-  const [isHovering, setIsHovering] = useState(false);
   const navigate = useNavigate();
 
   const toggleNavbar = () => {
@@ -17,35 +15,20 @@ function Navbar() {
     navigate('/');
   };
 
-  const handleMouseEnter = () => {
-    setIsHovering(true);
-  };
-
-  const handleMouseLeave = () => {
-    setIsHovering(false);
-  };
-
   return (
     <div className="navbar">
       <div className="leftSide" id={openLinks ? "open" : "close"}>
-        <img 
-          src={isHovering ? CompanyLogoHover : CompanyLogo} 
-          alt="Org-Logo" 
-          className="navbar-logo" 
-          onClick={navigateHome} 
-          onMouseEnter={handleMouseEnter} 
-          onMouseLeave={handleMouseLeave}
-        />
+        <img src={CompanyLogo} alt="Org-Logo" className="navbar-logo" onClick={navigateHome} />
         <div className="hiddenLinks">
           <Link to="/"> Home </Link>
           <Link to="/about"> About </Link>
-          <Link to="/ethics"> Submit Question </Link>
+          <Link to="/ethics"> Ethics </Link>
         </div>
       </div>
       <div className="rightSide">
         <Link to="/"> Home </Link>
         <Link to="/about"> About </ Link>
-        <Link to="/ethics"> Submit Question </ Link>
+        <Link to="/ethics"> Ethics </ Link>
         <button onClick={toggleNavbar}></button>
       </div>
     </div>
